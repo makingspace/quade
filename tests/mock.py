ed8ad2da0338051bb1115957871c660083010aaf
Paul Baranay <pbaranay@makespace.com>
2017-12-23 14:09:00 -0500

    Allow arbitrary functions to be registered through the QaMock

--- tests/mock.py
+++ tests/mock.py
@@ -3,24 +3,28 @@ from six import wraps
 from .fixtures import customer, staff_user
 
 
+class Default(object):
+    pass
+
+
 class QaMock:
 
     """
     Monkey patch the FixtureManager's registry, and automatically unapply the patch afterwards.
     """
 
-    def __init__(self, mdle):
+    def __init__(self, mdle, funcs=Default):
         self.module = mdle
+        if funcs == Default:
+            funcs = [customer, staff_user]
+        self.funcs = funcs
 
     def __call__(self, fn):
         @wraps(fn)
         def wrapper(*args, **kwargs):
             original_registry = self.module.manager._registry
             # Monkey patch the registry.
-            self.module.manager._registry = {
-                'customer': customer,
-                'staff_user': staff_user,
-            }
+            self.module.manager._registry = {func.__name__: func for func in self.funcs}
 
             # Run the wrapped function (e.g. a test).
             wrapped_fn_return_value = fn(*args, **kwargs)
